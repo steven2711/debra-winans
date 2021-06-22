7a1daa98bcdc4b3964381206b174b7a3d14d2219
Steven Watson <swatson.emu@gmail.com>
2021-06-21 19:27:33 -0500

    Added link to personal website in footer.

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,15 @@
-import styles from '@/styles/Footer.module.css'
+import styles from "@/styles/Footer.module.css";
 
 export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <p>Copyright 2021</p>
-        </footer>
-    )
+  return (
+    <footer className={styles.footer}>
+      <p>Copyright 2021</p>
+      <p>
+        Built by{" "}
+        <a href="https://www.stevenraywatson.com" target="_blank">
+          Cyber Dojo
+        </a>
+      </p>
+    </footer>
+  );
 }

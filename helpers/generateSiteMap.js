27db42aa22bbab2a42cb846c7049af582f31708e
Steven Watson <swatson.emu@gmail.com>
2021-06-27 15:00:40 -0400

    Sitemap error test 1

--- helpers/generateSiteMap.js
+++ helpers/generateSiteMap.js
@@ -9,27 +9,26 @@ async function generateSiteMap() {
     "!pages/api",
   ]);
 
-  const sitemap = `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-          ${pages
-            .map((page) => {
-              const path = page
-                .replace("pages", "")
-                .replace(".js", "")
-                .replace(".md", "");
-              const route = path === "/index" ? "" : path;
-              return `
-                      <url>
-                          <loc>${`https://debradwinans.com${route}`}</loc>
-                          <lastmod>${new Date().toISOString()}</lastmod>
-                          <changefreq>monthly</changefreq>
-                          <priority>1.0</priority>
-                      </url>
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pages
+      .map((page) => {
+        const path = page
+          .replace("pages", "")
+          .replace(".js", "")
+          .replace(".md", "");
+        const route = path === "/index" ? "" : path;
+        return `
+                <url>
+                    <loc>${`https://debradwinans.com${route}`}</loc>
+                    <lastmod>${new Date().toISOString()}</lastmod>
+                    <changefreq>monthly</changefreq>
+                    <priority>1.0</priority>
+                </url>
                   `;
-            })
-            .join("")}
-      </urlset>
+      })
+      .join("")}
+</urlset>
   `;
 
   fs.writeFileSync("public/sitemap.xml", sitemap);

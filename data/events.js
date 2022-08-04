5a1510a7a889f3ca6cc5158effdcc3e021c467ae
Steven Watson <swatson.emu@gmail.com>
2022-08-04 17:59:10 -0500

    Removed an event.

--- data/events.js
+++ data/events.js
@@ -1,19 +1,19 @@
 import { slugify } from "../helpers";
 
 export const events = [
-  {
-    id: 0,
-    smallImagePath: "/images/debra-flyer.jpg",
-    imagePath: "/images/debra-flyer.jpg",
-    title: "life altered not over masterclass",
-    titleCaps: "Life Altered Not Over Masterclass",
-    startDate: "March 1, 2022",
-    endDate: "March 29, 2022",
-    time: "7:00pm - 9:00pm EST",
-    days: "Tuesdays",
-    price: "$90",
-    purchasePath: "https://square.link/u/U6GcUa8T",
-    excerpt:
-      "Come join Dr. Debra D. Winans, author of 'Life, Altered Not Over', as she goes through each chapter in her book to discover God's will revealing truth with practical tools and relevant wisdom.",
-  },
+  // {
+  //   id: 0,
+  //   smallImagePath: "/images/debra-flyer.jpg",
+  //   imagePath: "/images/debra-flyer.jpg",
+  //   title: "life altered not over masterclass",
+  //   titleCaps: "Life Altered Not Over Masterclass",
+  //   startDate: "March 1, 2022",
+  //   endDate: "March 29, 2022",
+  //   time: "7:00pm - 9:00pm EST",
+  //   days: "Tuesdays",
+  //   price: "$90",
+  //   purchasePath: "https://square.link/u/U6GcUa8T",
+  //   excerpt:
+  //     "Come join Dr. Debra D. Winans, author of 'Life, Altered Not Over', as she goes through each chapter in her book to discover God's will revealing truth with practical tools and relevant wisdom.",
+  // },
 ];

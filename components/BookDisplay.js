ab602e42da2982ec689e6c246ef932a9d1d7cdee
Steven Watson <swatson.emu@gmail.com>
2022-08-10 13:59:35 -0500

    Removed book image

--- components/BookDisplay.js
+++ components/BookDisplay.js
@@ -6,12 +6,13 @@ export default function BookDisplay({ display }) {
   return (
     <section className={styles.bookDisplay}>
       <div className={styles.image}>
-        <Image
+        <p>REVISED ADDITION COMING SOON</p>
+        {/* <Image
           src="/images/book-cover.jpg"
           width={500}
           height={650}
           alt="Life Altered, Not Over! My Silent Journey to Wholeness"
-        />
+        /> */}
       </div>
       <div className={styles.info}>
         <div>
@@ -21,25 +22,16 @@ export default function BookDisplay({ display }) {
 
         <div>
           <p>
-            Nothing can inspire us more than a human story with which we can all
-            relate; a story that encourages us to believe in our own dreams and
-            to have faith in the promises of God who sometimes seems far away.
-            Life Altered Not Over! addresses the reality of pain and provides
-            essential tool for hurting individuals. People in any type of
-            emotional, mental and spiritual pain are looking for
-            “how-to-get-healed tools. This life changing book provides just
-            that; how to be healed, be free, be whole and to live your best
-            life! Order now to receive an autographed copy!
+            Nothing can inspire us more than a human story with which we can all relate; a story that encourages us to believe in our own dreams and to have
+            faith in the promises of God who sometimes seems far away. Life Altered Not Over! addresses the reality of pain and provides essential tool for
+            hurting individuals. People in any type of emotional, mental and spiritual pain are looking for “how-to-get-healed tools. This life changing book
+            provides just that; how to be healed, be free, be whole and to live your best life! Order now to receive an autographed copy!
           </p>
           <p>**Note** Amazon orders are not autographed.</p>
         </div>
 
         <div className={styles.btns}>
-          <Button
-            text="order now"
-            link={true}
-            path="https://checkout.square.site/merchant/MLH6RXNFN7NQ5/checkout/JQS4HKDONCUF26QAPOTTIUES"
-          />
+          <Button text="order now" link={true} path="https://checkout.square.site/merchant/MLH6RXNFN7NQ5/checkout/JQS4HKDONCUF26QAPOTTIUES" />
           {display ? (
             <Button
               text="order on amazon"
@@ -47,12 +39,7 @@ export default function BookDisplay({ display }) {
               path="https://www.amazon.com/LIFE-ALTERED-NOT-OVER-Wholeness/dp/1953443001/ref=sr_1_6?dchild=1&keywords=life+altered+not+over+book&qid=1623702537&sr=8-6"
             />
           ) : (
-            <Button
-              text="reflections"
-              link={true}
-              path="/life-altered-not-over"
-              btnDark={true}
-            />
+            <Button text="reflections" link={true} path="/life-altered-not-over" btnDark={true} />
           )}
         </div>
       </div>
